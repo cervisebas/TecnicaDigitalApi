252a1a0ff0c17ca4ce6893d85c8b18b5b2305ffa
Sebástian Cerviño García <cervisebas@gmail.com>
2023-02-01 10:59:08 -0300

    01/02/2023 10:59hs

---
+++ scripts/SecurityKeyCodes.php
@@ -0,0 +1,7 @@
+<?php
+    class SecurityKeyCodes {
+        public $keyCodeAdmin = '71b0@SVeX!m5kZ3XCe@6Uqfwtqu684SAyDsokT6x6$$Ov$5&Tl';
+        public $keyCodeConsole = 'lDgI5wbTeo5lr6exK4a9GF494sn2t3&#kIVV240sDX#zQvxq4W';
+        public $keyCodeFamily = 'g3rckKhS7D5ss^071wZT15Nb14SVP1ve*1O^bt#&YtwCeuv$#t';
+    }
+?>
